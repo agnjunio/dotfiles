f47b3e565a80998c050d2661574f5493bba46a7e
Agnaldo Junior <agnaldo.junior01@gmail.com>
2021-02-10 08:25:57 -0400

    Make slideshow sensible to xdg-user-dirs
    Signed-off-by: Agnaldo Junior <agnaldo.junior01@gmail.com>

--- .config/i3/slideshow-bg.sh
+++ .config/i3/slideshow-bg.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 while true; do
-  feh -z --bg-fill --no-fehbg $HOME/Pictures/Wallpappers/*
+  feh -z --bg-fill --no-fehbg $(xdg-user-dir PICTURES)/Wallpappers/*
   sleep 600
 done
