2cde68e7808ed9e77999a62df5ea7bbb82153365
Agnaldo Junior <agnaldo.junior01@gmail.com>
2023-06-02 12:02:34 -0400

    Swap vim splits with ease keys

--- .config/nvim/lua/anjek/splits.lua
+++ .config/nvim/lua/anjek/splits.lua
@@ -3,3 +3,9 @@ vim.keymap.set("n", "<C-W><C-Left>", "<C-w>>")
 vim.keymap.set("n", "<C-W><C-Right>", "<C-w><")
 vim.keymap.set("n", "<C-W><C-Up>", "<C-w>+")
 vim.keymap.set("n", "<C-W><C-Down>", "<C-w>-")
+
+-- Swap splits with ease
+vim.keymap.set("n", "<C-W><S-Left>", "<C-w>H")
+vim.keymap.set("n", "<C-W><S-Right>", "<C-w>L")
+vim.keymap.set("n", "<C-W><S-Up>", "<C-w>K")
+vim.keymap.set("n", "<C-W><S-Down>", "<C-w>J")
