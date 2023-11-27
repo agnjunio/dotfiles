a7fac3dd61b1bd283c868635f10036ab30fb343f
Agnaldo Junior <agnaldo.junior01@gmail.com>
2023-11-27 02:20:10 -0400

    Add noice to nvim for a revamped cmdline and popup experience

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -80,4 +80,56 @@ return {
       })
     end,
   },
+
+  -- Revamp messages, cmdline and popupmenus
+  {
+    "folke/noice.nvim",
+    event = "BufRead",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    opts = {
+      presets = {
+        bottom_search = true,          -- use a classic bottom cmdline for search
+        command_palette = false,       -- position the cmdline and popupmenu together
+        long_message_to_split = false, -- long messages will be sent to a split
+        inc_rename = false,            -- enables an input dialog for inc-rename.nvim
+      },
+      routes = {
+        { view = "cmdline", filter = { event = "msg_showmode" } },                                           -- Show recording in notify
+        { view = nil,       filter = { event = "msg_show", kind = "search_count" }, opts = { skip = true } } -- Hide search text
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = 2,
+            col = "50%",
+          },
+          size = {
+            width = 100,
+            height = "auto",
+          }
+        },
+        popupmenu = {
+          relative = "editor",
+          position = {
+            row = 5,
+            col = "50%",
+          },
+          size = {
+            width = 100,
+            height = "50%",
+          },
+          border = {
+            style = "rounded",
+            padding = { 0, 1 },
+          },
+          win_options = {
+            winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" }
+          }
+        }
+      },
+    },
+  }
 }
