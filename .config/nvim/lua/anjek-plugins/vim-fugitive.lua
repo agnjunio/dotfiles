2747e01d854b95981d3083fb6f8618c0b4dd9124
Agnaldo Junior <agnaldo.junior01@gmail.com>
2024-03-22 11:56:30 -0400

    Add git blame command

--- .config/nvim/lua/anjek-plugins/vim-fugitive.lua
+++ .config/nvim/lua/anjek-plugins/vim-fugitive.lua
@@ -2,6 +2,7 @@ return {
   'tpope/vim-fugitive',
   lazy = true,
   keys = {
-    { "<leader>gs", "<cmd>Git<CR>", desc = "Git Status" },
+    { "<leader>gs", "<cmd>Git<CR>",       desc = "Git status" },
+    { "<leader>gb", "<cmd>Git blame<CR>", desc = "Git blame" },
   },
 }
