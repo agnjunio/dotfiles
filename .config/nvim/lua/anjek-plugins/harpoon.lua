466d95da2bf8f5e715d9cf5ae99dd9933c3c1f5f
Agnaldo Junior <agnaldo.junior01@gmail.com>
2024-05-05 06:50:59 -0400

    Fix harpoon keys

--- .config/nvim/lua/anjek-plugins/harpoon.lua
+++ .config/nvim/lua/anjek-plugins/harpoon.lua
@@ -2,7 +2,7 @@ return {
   'ThePrimeagen/harpoon',
   lazy = true,
   keys = {
-    { '<leader>a', function() require("harpoon.mark").add_file() end },
-    { '<C-e>',     function() require("harpoon.ui").toggle_quick_menu() end },
+    { '<leader>fa', function() require("harpoon.mark").add_file() end,        desc = "Add file to Harpoon" },
+    { '<C-e>',      function() require("harpoon.ui").toggle_quick_menu() end, desc = "Open Harpoon" },
   },
 }
