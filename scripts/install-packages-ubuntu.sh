f6c6c11a89a3bffe6c47b98ee8481a2258e86222
Agnaldo Junior <agnaldo.junior01@gmail.com>
2025-10-27 10:20:21 -0400

    Update ubuntu install script to support WSL

--- scripts/install-packages-ubuntu.sh
+++ scripts/install-packages-ubuntu.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+is_wsl() {
+    if grep -qi microsoft /proc/version 2>/dev/null; then
+        return 0
+    fi
+    return 1
+}
+
 packages=(
     bison
     clang
@@ -15,28 +22,52 @@ packages=(
     neovim
     ripgrep
     tig
+    terraform
     tmux
     zsh
 )
 
-# Let's do it!
-install_packages() {
-    local packages=( $@ )
-    echo "## Installing dependencies..."
-    sudo apt install ${packages[@]} -y
+# Non-WSL only packages
+if ! is_wsl; then
+    packages+=(
+        docker-ce
+        docker-ce-cli
+        containerd.io
+        docker-buildx-plugin
+        docker-compose-plugin
+    )
+fi
+
+add_repositories() {
+    # Ensure minimal tools are installed
+    sudo apt update
+    sudo apt install -y install ca-certificates curl gnupg
+
+    # Add docker repository only if not in WSL
+    if ! is_wsl; then
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    fi
+
+    # Add Hashicorp repository
+    wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 }
+
+echo "## Adding repositories..."
+add_repositories
 
 echo '## Updating repositories...'
 sudo apt update
-install_packages ${packages[@]}
 
-echo "## Installing oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "## Installing packages..."
+sudo apt install ${packages[@]} -y
 
-echo "## Installing volta (Node Version Manager)"
-curl https://get.volta.sh | bash
-
-echo "## Installing gvm (Go Version Manager)"
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+if ! omz version &>/dev/null; then 
+    echo "## Installing oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 echo "## Installation complete. You can now run the sync.sh script"
